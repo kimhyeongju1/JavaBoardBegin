651527e2f150653d369757a65cbaba93670ed58a
형주 김 <heci32@naver.com>
2024-01-24 23:06:18 +0900

    게시판1

---
+++ src/main/java/com/codingrecipe/member/controller/MemberController.java
@@ -0,0 +1,62 @@
+package com.codingrecipe.member.controller;
+
+import com.codingrecipe.member.dto.MemberDTO;
+import com.codingrecipe.member.service.MemberService;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+    // 생성자 주입
+    private final MemberService memberService;
+
+    //회원가입 페이지 출력 요청
+    @GetMapping("/member/save")
+    public String saveForm(){
+        return "save";
+    }
+
+    @PostMapping("/member/save")
+    public String save(@ModelAttribute MemberDTO memberDTO) {
+        System.out.println("memberDTO = " + memberDTO);
+        System.out.println("MemberController.save");
+        memberService.save(memberDTO);
+        return "login";
+    }
+
+    @GetMapping("/member/login")
+    public String loginForm() {
+        return "login";
+    }
+
+    @PostMapping("/member/login")
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
+        MemberDTO loginResult = memberService.login(memberDTO);
+        if(loginResult != null) {
+            // login 성공
+            session.setAttribute("loginEmail",loginResult.getMemberEmail());
+            return "main";
+        } else {
+            //login 실패
+            return "login";
+        }
+    }
+
+    @GetMapping("/member/")
+    public String findAll(Model model) {
+        List<MemberDTO> memberDTOList = memberService.findAll();
+        // 어떠한 html로 가져갈 데이터가 있다면 model을 사용.
+        model.addAttribute("memberList", memberDTOList);
+        return "list";
+    }
+
+}
---
+++ src/main/java/com/codingrecipe/member/entity/MemberEntity.java
@@ -0,0 +1,34 @@
+package com.codingrecipe.member.entity;
+
+import com.codingrecipe.member.dto.MemberDTO;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+@Table(name = "member_table")
+public class MemberEntity {
+    @Id // pk지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//Auto increment
+    private long id;
+
+    @Column(unique = true) //unique 제약 조건 추가
+    private String memberEmail;
+
+    @Column
+    private String memberPassword;
+
+    @Column
+    private String memberName;
+
+    public static MemberEntity toMemberEntity(MemberDTO memberDTO) {
+        MemberEntity memberEntity = new MemberEntity();
+        memberEntity.setMemberEmail(memberDTO.getMemberEmail());
+        memberEntity.setMemberName(memberDTO.getMemberName());
+        memberEntity.setMemberPassword(memberDTO.getMemberPassword());
+
+        return memberEntity;
+    }
+}
